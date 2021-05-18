4de73c20fc214651bcba60fef0c9c1639dcfc57f
Muhammad-Jibril <jbashir52@gmail.com>
2021-05-18 16:29:24 +0800

    update

---
+++ lib/utils/i18n/index.dart
@@ -0,0 +1,21 @@
+import 'package:app/utils/i18n/en/account.dart';
+import 'package:app/utils/i18n/en/assets.dart';
+import 'package:app/utils/i18n/en/profile.dart';
+import 'package:app/utils/i18n/zh/account.dart';
+import 'package:app/utils/i18n/zh/assets.dart';
+import 'package:app/utils/i18n/zh/profile.dart';
+
+const Map<String, Map<String, Map<String, String>>> i18n_full_dic_app = {
+  'en': {
+    'account': enAccount,
+    'assets': enAssets,
+    'profile': enProfile,
+    'public': enPublic,
+  },
+  'zh': {
+    'account': zhAccount,
+    'assets': zhAssets,
+    'profile': zhProfile,
+    'public': zhPublic,
+  }
+};
