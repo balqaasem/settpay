2044a0d290c7237b23923b154c44f0c8b4066753
Muhammad-Jibril <jbashir52@gmail.com>
2021-05-18 16:38:54 +0800

    update

---
+++ lib/service/apiAssets.dart
@@ -0,0 +1,47 @@
+import 'package:settpay/service/index.dart';
+import 'package:settpay/service/walletApi.dart';
+
+class ApiAssets {
+  ApiAssets(this.apiRoot);
+
+  final AppService apiRoot;
+
+  Future<Map> updateTxs(int page) async {
+    apiRoot.store.assets.setTxsLoading(true);
+
+    final acc = apiRoot.keyring.current;
+    Map res = await apiRoot.subScan.fetchTransfersAsync(
+      acc.address,
+      page,
+      network: apiRoot.plugin.basic.name,
+    );
+
+    if (page == 0) {
+      apiRoot.store.assets.clearTxs();
+    }
+    // cache first page of txs
+    await apiRoot.store.assets.addTxs(
+      res,
+      acc,
+      apiRoot.plugin.basic.name,
+      shouldCache: page == 0,
+    );
+
+    apiRoot.store.assets.setTxsLoading(false);
+    return res;
+  }
+
+  Future<void> fetchMarketPrice() async {
+    if (apiRoot.plugin.basic.isTestNet) return;
+
+    final res = await WalletApi.getTokenPrice(apiRoot.plugin.basic.name);
+    // ignore: unnecessary_null_comparison
+    if (res == null || res['data'] == null) {
+      print('fetch market price failed');
+      return;
+    }
+    final symbol = res['data']['token'][0];
+    apiRoot.store.assets
+        .setMarketPrices(symbol, res['data']['detail'][symbol]['price']);
+  }
+}
