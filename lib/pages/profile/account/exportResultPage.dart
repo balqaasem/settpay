e8e3ffec6f4f4d3a9b4d974312962ff8eaff1423
Muhammad-Jibril <jbashir52@gmail.com>
2021-05-20 01:43:35 +0800

    rm unused import

--- lib/pages/profile/account/exportResultPage.dart
+++ lib/pages/profile/account/exportResultPage.dart
@@ -1,7 +1,6 @@
 import 'package:settpay/utils/i18n/index.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 import 'package:settpay_sdk/storage/types/keyPairData.dart';
 import 'package:settpay_sdk/utils/i18n.dart';
